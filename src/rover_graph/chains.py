c4af0d94356c434e534ec69c519ac6402b034317
Gaurab <gaurav.majumdar10@gmail.com>
2026-08-20 07:26:42 +0530

    Reflexion agent

--- src/rover_graph/chains.py
+++ src/rover_graph/chains.py
@@ -1,31 +1,19 @@
 import os
+import datetime
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_core.output_parsers.openai_tools import (
+    JsonOutputToolsParser,
+    PydanticToolsParser
+)
+
+from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
+from .schemas import AnswerQuestion, ReviseAnswer
 
 MODEL="nvidia/nemotron-nano-9b-v2:free"
-
-reflection_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet."
-            "Always provide detailed recommendations, including requests for length, virality, style, etc.",
-        ),
-        MessagesPlaceholder(variable_name="messages"),
-    ]
-)
-
-generation_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts."
-            " Generate the best twitter post possible for the user's request."
-            " If the user provides critique, respond with a revised version of your previous attempts.",
-        ),
-        MessagesPlaceholder(variable_name="messages"),
-    ]
-)
 
 llm = ChatOpenAI(
     model=MODEL,
@@ -34,5 +22,43 @@ llm = ChatOpenAI(
     temperature=0
 )
 
-generation_chain = generation_prompt | llm
-reflection_chain = reflection_prompt | llm
+parser = JsonOutputToolsParser(return_id=True)
+parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
+
+actor_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are an expert researcher.
+Current time: {time}
+
+1. {first_instruction}
+2. Reflect and crtique your answer. Be severe to maximize improvement.
+3. Recommend search queries to research information and improve your answer. """,
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+        ("system","Answer the User's question above using the required format"),
+    ]
+).partial(time=lambda: datetime.datetime.now().isoformat(),
+)
+
+first_responder_prompt_template = actor_prompt_template.partial(
+    first_instruction="Provide a detailed ~200 words answer."
+)
+
+first_responder_chain = first_responder_prompt_template | llm.bind_tools(
+    tools=[AnswerQuestion], tool_choice="AnswerQuestion"
+)
+
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
+
+revisor_chain = actor_prompt_template.partial(
+    first_instruction=revise_instructions
+) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
